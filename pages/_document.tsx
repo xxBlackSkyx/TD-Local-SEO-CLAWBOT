e6691e9c56f875e22e6cf35c6b08db7dc866e925
Your Name <your-email@example.com>
2026-04-29 18:15:22 -0400

    Add Google Search Console verification meta tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta name="google-site-verification" content="oir82bMtK-ZG2xglL8R2H1oKGOGZI5vy7ILywxhs02U" />
         <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
         <meta name="theme-color" content="#667eea" />
       </Head>
